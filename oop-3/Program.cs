3e9cffc2fcef205d9aad97b60dfdbddc13451711
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-07-15 14:46:03 +0300

    binding & not binding

--- oop-3/Program.cs
+++ oop-3/Program.cs
@@ -7,19 +7,19 @@ namespace oop_3
     internal class Program
     {
         #region overloading
-        public static int sum(int x, int y)
-        { return x + y; }
+        //public static int sum(int x, int y)
+        //{ return x + y; }
       
-        public static int sum(int x, int y, int z)
-        { return x + y + z; }
+        //public static int sum(int x, int y, int z)
+        //{ return x + y + z; }
 
-        public static double sum(double x, double y)
-        { return x + y  ; }
+        //public static double sum(double x, double y)
+        //{ return x + y  ; }
 
-        public static double sum(int x, double y)
-        { return x + y; }
-        public static double sum(double x, int y)
-        { return x + y; }
+        //public static double sum(int x, double y)
+        //{ return x + y; }
+        //public static double sum(double x, int y)
+        //{ return x + y; }
         #endregion
         static void Main(string[] args)
         {
@@ -38,19 +38,48 @@ namespace oop_3
             //Console.WriteLine();
             #endregion
             #region [override]
-            typeA typeA = new typeA(1);
-                typeA.A = 10;
-            typeB typeB = new typeB(1,2);
-            typeB.A = 10;
-            typeB.B = 20;
-            typeB.MyFun01();
-            typeB.MyFun02();
+            //typeA typeA = new typeA(1);
+            //    typeA.A = 10;
+            //typeB typeB = new typeB(1,2);
+            //typeB.A = 10;
+            //typeB.B = 20;
+            //typeB.MyFun01();
+            //typeB.MyFun02();
 
 
 
 
             #endregion
-            #region
+            #region Binding
+            //typeA refbase;
+            //refbase = new typeB(1,2);
+            //refbase.A = 10;
+            ////refbase.B = 10; invalid
+
+            //refbase.MyFun01();// I am Base[parent]
+            //refbase.MyFun02();// TypeB: A = 10 , B = 2
+
+
+
+            #endregion
+            #region Not Binding
+            //typeB childref = new typeA();   invalid
+            //typeB childref = (typeB) new typeA(1); // not binding /explicit casting / unsafe casting
+           
+            
+            //typeA typeA = new typeA(1);
+            //typeB  typeB = (typeB) typeA;
+
+            //Console.WriteLine(typeB.A);
+            //Console.WriteLine(typeB.B);
+
+
+            typeA typeA = new typeB(1,2);
+            typeB typeB = (typeB)typeA;
+
+            Console.WriteLine(typeB.A);
+            Console.WriteLine(typeB.B);
+
             #endregion
             #region
             #endregion
